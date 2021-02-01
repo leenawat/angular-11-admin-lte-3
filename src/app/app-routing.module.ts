578942a83d2e24458cca54a19a51f6a97bc7e0fd
Leenawat Papahom <leenawat.pa@gmail.com>
2021-02-01 16:48:46 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: MainComponent,
+  children: [
+    { path: '', component: HomeComponent },
+    { path: 'contacts', component: ContactsComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
+import { ControlSidebarComponent } from './main/control-sidebar/control-sidebar.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ControlSidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
